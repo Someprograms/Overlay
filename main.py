469c432ce523f4260c1733b6f29926c108ccebdb
Someprograms <arttu.nykanen06@gmail.com>
2024-09-15 19:59:12 +0300

    Version 1

--- main.py
+++ main.py
@@ -1,90 +1,81 @@
-import time
-
-import keyboard
-import re
 import tkinter as tk
 import threading
 from pynput import keyboard as Keyboard
-selectedKeys = ['a', 'b']
+selectedKeys = []
 forloopIndex = 0
 startCanvasThread = True
 startCanvas2Thread = True
 
 
 def on_key_release(key):
-
-    if key.char == selectedKeys[0]:
-        canvas.config(bg='white')
-    elif key.char == selectedKeys[1]:
-        canvas2.config(bg='white')
-
-
-def changecanvascolor():
-    canvas.config(bg='black')
-    #time.sleep(0.1)
-    #canvas.config(bg='white')
+    if selectedKeys:
+        try:
+            if key.char == selectedKeys[0]:
+                canvas.delete(text_id)
+            elif key.char == selectedKeys[1]:
+                canvas2.delete(text_id2)
+        except:
+            print('')
 
 
-
-def changecanvascolor2():
-    canvas2.config(bg='black')
-    #time.sleep(0.1)
-    #canvas2.config(bg='white')
-
-
-
-
-def detectkeypress():
-    print("text")
-    while True:
-        for i in range(len(selectedKeys)):
-            val = selectedKeys[i]
-
-            if keyboard.is_pressed(val) and i == 0:
-                changecanvascolor()
-                #t3 = threading.Thread(target=changecanvascolor)
-                #if not t3.is_alive():
-                    #t3.start()
-                    #t3.join()
-            elif keyboard.is_pressed(val) and i == 1:
-                changecanvascolor2()
-
-            #elif keyboard.release(val) and i == 0:
-                #canvas.config(bg='white')
-            #elif keyboard.release(val) and i == 1:
-                #canvas2.config(bg='white')
-                #if startCanvas2Thread:
-                    #t4 = threading.Thread(target=changecanvascolor2)
-
-                    #if not t4.is_alive():
-                        #t4.start()
-                        #t4.join()
-
+def on_key_press(key):
+    if selectedKeys:
+        try:
+            if key.char == selectedKeys[0]:
+                global text_id
+                text_id = canvas.create_text(25,25, text=selectedKeys[0].upper(), font='Rosemary 15')
+            elif key.char == selectedKeys[1]:
+                global text_id2
+                text_id2 = canvas2.create_text(25, 25, text=selectedKeys[1].upper(), font='Rosemary 15')
+        except:
+            print('')
 
 
 
 
 def window():
-    listener =  Keyboard.Listener(on_release=on_key_release)
+    listener = Keyboard.Listener(on_release=on_key_release, on_press=on_key_press)
     listener.start()
-
     win = tk.Tk()
     global canvas
-    canvas = tk.Canvas(win, bg='green')
-    canvas.grid(column=0, row=0)
+    canvas = tk.Canvas(win)
+    canvas.grid(column=2, row=0)
+    canvas.config(width=50, height=50)
+    global entry
+    entry = tk.Entry(win)
+    entry.grid(column=1, row=0)
+    entry.config()
+    text = tk.Text()
+    text.insert(index=0.0, chars='key 1')
+    text.grid(column=0, row=0)
+    text.config(width=10, height=1)
+    text2 = tk.Text()
+    text2.insert(index=0.0, chars='key 2')
+    win.overrideredirect(True)
+    text2.grid(column=0, row=1)
+    text2.config(width=10, height=1)
+    global entry2
+    entry2 = tk.Entry(win)
+    entry2.grid(column=1, row=1)
+    entry2.config()
     global canvas2
     canvas2 = tk.Canvas(win)
-    canvas2.grid(column=1, row=0)
-
+    canvas2.grid(column=3, row=0)
+    canvas2.config(width=50, height=50)
+    tk.Button(win, text="Set keys", command=append).grid(row=1, column=2, sticky='nsew')
+    win.wm_attributes("-topmost", 1)
     tk.mainloop()
 
 
-t2 = threading.Thread(target=detectkeypress)
+def append():
+    selectedKeys.clear()
+    selectedKeys.append(entry.get())
+    selectedKeys.append(entry2.get())
+    print(selectedKeys)
+
 t1 = threading.Thread(target=window)
-
-
-t2.start()
 t1.start()
+
 
 t1.join()
 
