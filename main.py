26d1006d80a6d6fae73828e8567b7a10f6003980
Someprograms <arttu.nykanen06@gmail.com>
2024-09-17 00:54:57 +0300

    update

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ selectedKeys = []
 forloopIndex = 0
 startCanvasThread = True
 startCanvas2Thread = True
-
+openOptions = False
+keys = 0
+global canvas2
 
 def on_key_release(key):
     if selectedKeys:
@@ -31,39 +33,87 @@ def on_key_press(key):
             print('')
 
 
+def OpenOptionsMenu():
+    global openOptions
+    openOptions = True
+
+class addOrRemoveKeyClass:
+
+
+
+    def addOrRemoveKey(self, addOrRemoveCanvas):
+        win.update_idletasks()
+        options1.update_idletasks()
+        global keys
+        global canvas2
+        if addOrRemoveCanvas == 'addCanvas':
+            keys = keys + 1
+        elif addOrRemoveCanvas == 'removeCanvas':
+            keys = keys - 1
+        print(keys)
+        if keys == 2:
+
+            canvas2 = tk.Canvas(win)
+            canvas2.grid(column=2, row=0)
+            canvas2.config(width=50, height=50)
+        elif keys == 1:
+            try:
+                canvas2.destroy()
+                del canvas2
+                print('canvas2 has been destroyed')
+            except:
+                print('canvas2 is undefined')
+            print('canvas')
+            global canvas
+            canvas = tk.Canvas(win)
+            canvas.grid(column=1, row=0)
+            canvas.config(width=50, height=50)
+        else:
+            print('')
+
+
+
+
+def removekey():
+
+    global keys
+    keys = keys - 1
+    print(keys)
 
 
 def window():
     listener = Keyboard.Listener(on_release=on_key_release, on_press=on_key_press)
     listener.start()
+    global win
     win = tk.Tk()
-    global canvas
-    canvas = tk.Canvas(win)
-    canvas.grid(column=2, row=0)
-    canvas.config(width=50, height=50)
-    global entry
-    entry = tk.Entry(win)
-    entry.grid(column=1, row=0)
-    entry.config()
-    text = tk.Text()
-    text.insert(index=0.0, chars='key 1')
-    text.grid(column=0, row=0)
-    text.config(width=10, height=1)
-    text2 = tk.Text()
-    text2.insert(index=0.0, chars='key 2')
+
     win.overrideredirect(True)
-    text2.grid(column=0, row=1)
-    text2.config(width=10, height=1)
-    global entry2
-    entry2 = tk.Entry(win)
-    entry2.grid(column=1, row=1)
-    entry2.config()
-    global canvas2
-    canvas2 = tk.Canvas(win)
-    canvas2.grid(column=3, row=0)
-    canvas2.config(width=50, height=50)
-    tk.Button(win, text="Set keys", command=append).grid(row=1, column=2, sticky='nsew')
     win.wm_attributes("-topmost", 1)
+
+    def options():
+        global options1
+        options1 = tk.Toplevel(win)
+        tk.Button(options1, text="Set keys", command=append).grid(row=0, column=2, sticky='nsew')
+        tk.Button(options1, text="Add key", command=lambda x='addCanvas', y='': addOrRemoveKeyClass.addOrRemoveKey(addOrRemoveCanvas=x,self=y)).grid(row=1, column=2, sticky='nsew')
+        tk.Button(options1, text="Remove key", command=lambda x='removeCanvas', y='': addOrRemoveKeyClass.addOrRemoveKey(addOrRemoveCanvas=x, self=y)).grid(row=2, column=2, sticky='nsew')
+        text = tk.Text(options1)
+        text.insert(index=0.0, chars='key 1')
+        text.grid(column=0, row=0)
+        text.config(width=10, height=1)
+        text2 = tk.Text(options1)
+        text2.insert(index=0.0, chars='key 2')
+        text2.grid(column=0, row=1)
+        text2.config(width=10, height=1)
+        global entry
+        entry = tk.Entry(options1)
+        entry.grid(column=1, row=0)
+        entry.config()
+        global entry2
+        entry2 = tk.Entry(options1)
+        entry2.grid(column=1, row=1)
+        entry2.config()
+    tk.Button(win, text="Settings", command=options).grid(row=0, column=0, sticky='nsew')
+    win.update_idletasks()
     tk.mainloop()
 
 
